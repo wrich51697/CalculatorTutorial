531284cacac4be880ab7f3119577d028ab82dd9b
Brian Richmond <Brian Richmond@BRIANS-DESKTOP>
2022-07-20 08:44:21 -0400

    Add project files.

---
+++ Calculator.cpp
@@ -0,0 +1,20 @@
+#include "Calculator.h"
+
+#include "Calculator.h"
+
+double Calculator::Calculate(double x, char oper, double y)
+{
+    switch (oper)
+    {
+    case '+':
+        return x + y;
+    case '-':
+        return x - y;
+    case '*':
+        return x * y;
+    case '/':
+        return x / y;
+    default:
+        return 0.0;
+    }
+}
